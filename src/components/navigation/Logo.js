4e508ddaca81f19e3cccc8b17ef7742f6dc6db31
Zombie_Muse <gjmccormack@cpp.edu>
2022-12-29 07:03:08 -0800

    Made a temp logo

--- src/components/navigation/Logo.js
+++ src/components/navigation/Logo.js
@@ -2,11 +2,9 @@ import React from 'react';
 
 const Logo = () => {
   return (
-    <div className="bg-gray-400 h-24   w-24 -mb-16 rounded-full text-center">
-      <div className="p-4 font-bold text-[24] ">
-        Quail <br />
-        Creek
-      </div>
+    <div className="bg-gray-400 static h-24 w-24 -mb-16 rounded-full text-center items-center">
+      <div className="-left-3 top-4 p-4 font-bold text-3xl absolute">Quail</div>
+      <div className="left-2 p-10 font-bold text-3xl absolute">Creek</div>
     </div>
   );
 };
